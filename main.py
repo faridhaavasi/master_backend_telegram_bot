e3a13e1dc13483f5fa1fd3c518b69660ba4c31cf
faridhavasi <105421842+faridhaavasi@users.noreply.github.com>
2025-02-04 17:26:52 +0330

    Implement user registration flow in Telegram bot with multi-step input for personal details

--- main.py
+++ main.py
@@ -15,7 +15,46 @@ bot = TeleBot(API_KEY)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, 'Hello, I am a bot that can help you with your tasks. What is your name?')
+    bot.send_message(message.chat.id, 'خوش آمدید لطفا /register را بزنید')
+
+
+@bot.message_handler(commands=['register'])
+def set_first_name(message, first_name=None):
+    bot.send_message(message.chat.id, 'لطفا نام خود را وارد کنید')
+    bot.register_next_step_handler(message, callback=set_last_name, first_name=message.text)
+
+def set_last_name(message, first_name):
+    bot.send_message(message.chat.id, 'لطفا نام خانوادگی خود را وارد کنید')
+    bot.register_next_step_handler(message, callback=set_phone, first_name=first_name, last_name=message.text)
+
+def set_phone(message, first_name, last_name):
+    bot.send_message(message.chat.id, 'لطفا شماره تماس خود را وارد کنید')
+    bot.register_next_step_handler(message, callback=set_rool, first_name=first_name, 
+                                   last_name=last_name, 
+                                   phone=message.text)
+
+def set_rool(message, first_name, last_name, phone):
+    bot.send_message(message.chat.id, 'لطفا نقش خود را انتخاب کنید')
+    bot.send_message(message.chat.id, '1- مستر')
+    bot.send_message(message.chat.id, '2- عادی')
+    bot.register_next_step_handler(message, callback=set_status_work, first_name=first_name,
+                                   last_name=last_name, phone=phone, rool=message.text)
+
+
+def set_status_work(message, first_name, last_name, phone, rool):
+    bot.send_message(message.chat.id, 'لطفا وضعیت کاری خود را انتخاب کنید')
+    bot.send_message(message.chat.id, '1-شاغل')
+    bot.send_message(message.chat.id, '2- بیکار')
+    bot.register_next_step_handler(message, callback=finish_register, first_name=first_name,
+                                   last_name=last_name, phone=phone, rool=rool, status_work=message.text)
+
+def finish_register(message, first_name, last_name, phone, rool, status_work):
+    rool = Rool.create(name=rool)
+    user = User.create(first_name=first_name, last_name=last_name, phone=phone, rool=rool, 
+                       status_work=str(status_work))
+    user.save()
+   
+    bot.send_message(message.chat.id, 'ثبت نام شما با موفقیت انجام شد')
 
 
 
--- models.py
+++ models.py
@@ -15,7 +15,7 @@ class User(Model):
     last_name = CharField()
     phone = CharField() # This field is a string that will hold the phone number.
     rool = ForeignKeyField(Rool, backref='rools') # This field is a foreign key that will hold the rool of the user.
-    status = BooleanField(default=False) # This field is a boolean that will hold the status of the user.
+    status_work = CharField() # This field is a string that will hold the status of the user.
     
     class Meta:
         database = db # This model uses the "people.db" database.
